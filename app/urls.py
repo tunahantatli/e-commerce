c02eb4e5ac7cc234f6f34f075140061ec6c3d745
Tunahan Tatli <tunahantatl@gmail.com>
2023-05-10 13:18:23 +0300

    e3

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
+    path("", views.home, name="home-page"),
+    path("about/", views.about, name="about-page"),
+    path("contact/", views.contact, name="contact-page"),
 ]
