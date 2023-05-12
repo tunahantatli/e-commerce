f545936a35987d3cc613bffd7d49e8dbbda141ed
Tunahan Tatli <tunahantatl@gmail.com>
2023-05-12 18:17:26 +0300

    e5

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path("", views.home, name="home-page"),
     path("about/", views.about, name="about-page"),
     path("contact/", views.contact, name="contact-page"),
     path("category/<slug:val>", views.CategoryView.as_view(), name="category")
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ec/settings.py
+++ ec/settings.py
@@ -125,6 +125,7 @@ MEDIA_ROOT = BASE_DIR/'media'
 
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
