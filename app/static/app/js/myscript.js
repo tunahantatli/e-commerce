8ad021c78b934feb8c0219b68757f8b26292cd73
Tunahan Tatli <tunahantatl@gmail.com>
2023-05-22 10:50:00 +0300

    v8-e2

--- app/static/app/js/myscript.js
+++ app/static/app/js/myscript.js
@@ -1,119 +1,58 @@
-$('#slider1, #slider2, #slider3').owlCarousel({
-    loop: true,
-    margin: 20,
-    responsiveClass: true,
-    responsive: {
-        0: {
-            items: 2,
-            nav: false,
-            autoplay: true,
-        },
-        600: {
-            items: 4,
-            nav: true,
-            autoplay: true,
-        },
-        1000: {
-            items: 6,
-            nav: true,
-            loop: true,
-            autoplay: true,
-        }
+// Get references to the elements
+const minusButtons = document.querySelectorAll('.minus-cart');
+const plusButtons = document.querySelectorAll('.plus-cart');
+const removeButtons = document.querySelectorAll('.remove-cart');
+const quantityElements = document.querySelectorAll('#quantity');
+const amountElement = document.getElementById('amount');
+const totalAmountElement = document.getElementById('totalamount');
+
+// Add event listeners to minus buttons
+minusButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const productId = button.getAttribute('pid');
+    const quantityElement = document.querySelector(`#quantity[pid="${productId}"]`);
+    let quantity = parseInt(quantityElement.textContent);
+    if (quantity > 1) {
+      quantity--;
+      quantityElement.textContent = quantity;
+      updateTotalAmount();
     }
-})
-const n = ({ path: e }) => {
-    if (e && Array.isArray(e)) {
-      return e.map((e) => e.className).join(" ").includes("css-inspector-cta");
-    }
-    return false; // or handle the case where `e` is not defined or not an array
-  };
-  
-  if ("HTML" === t.tagName || "BODY" === t.tagName || n(e)) {
-    // Rest of your code
-  }
-  
+  });
+});
 
-$('.plus-cart').click(function(){
-    var id=$(this).attr("pid").toString();
-    var eml=this.parentNode.children[2] 
-    console.log("pid =", id)
-    $.ajax({
-        type:"GET",
-        url:"/pluscart/",
-        data:{
-            prod_id:id
-        },
-        success:function(data){
-            console.log("data =", data);
-            eml.innerText=data.quantity 
-            document.getElementById("amount").innerText=data.amount 
-            document.getElementById("totalamount").innerText=data.totalamount
-        }
-    })
-})
+// Add event listeners to plus buttons
+plusButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const productId = button.getAttribute('pid');
+    const quantityElement = document.querySelector(`#quantity[pid="${productId}"]`);
+    
+    let quantity = parseInt(quantityElement.textContent);
+    quantity++;
+    quantityElement.textContent = quantity;
+    updateTotalAmount();
+  });
+});
 
-$('.minus-cart').click(function(){
-    var id=$(this).attr("pid").toString();
-    var eml=this.parentNode.children[2] 
-    $.ajax({
-        type:"GET",
-        url:"/minuscart/",
-        data:{
-            prod_id:id
-        },
-        success:function(data){
-            eml.innerText=data.quantity;
-            document.getElementById("amount").innerText=data.amount;
-            document.getElementById("totalamount").innerText=data.totalamount;
-        }
-    })
-})
+// Add event listeners to remove buttons
+removeButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    const productId = button.getAttribute('pid');
+    const rowElement = button.closest('.row');
+    rowElement.remove();
+    updateTotalAmount();
+  });
+});
 
+// Function to update the total amount
+function updateTotalAmount() {
+  let totalAmount = 0;
+  quantityElements.forEach(element => {
+    const quantity = parseInt(element.textContent);
+    const priceElement = element.closest('.row').querySelector('strong');
+    const price = parseFloat(priceElement.textContent.replace('$ ', ''));
+    totalAmount += quantity * price;
+  });
 
-$('.remove-cart').click(function(){
-    var id=$(this).attr("pid").toString();
-    var eml=this
-    $.ajax({
-        type:"GET",
-        url:"/removecart",
-        data:{
-            prod_id:id
-        },
-        success:function(data){
-            document.getElementById("amount").innerText=data.amount; 
-            document.getElementById("totalamount").innerText=data.totalamount;
-            eml.parentNode.parentNode.parentNode.parentNode.remove() ;
-        }
-    })
-})
-
-
-$('.plus-wishlist').click(function(){
-    var id=$(this).attr("pid").toString();
-    $.ajax({
-        type:"GET",
-        url:"/pluswishlist",
-        data:{
-            prod_id:id
-        },
-        success:function(data){
-            //alert(data.message)
-            window.location.href = `http://localhost:8000/product-detail/${id}`
-        }
-    })
-})
-
-
-$('.minus-wishlist').click(function(){
-    var id=$(this).attr("pid").toString();
-    $.ajax({
-        type:"GET",
-        url:"/minuswishlist",
-        data:{
-            prod_id:id
-        },
-        success:function(data){
-            window.location.href = `http://localhost:8000/product-detail/${id}`
-        }
-    })
-})
+  amountElement.textContent = '$ ' + totalAmount.toFixed(2);
+  totalAmountElement.textContent = '$ ' + totalAmount.toFixed(2);
+}
